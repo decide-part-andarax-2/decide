1dfb0c0fe39fba5b8b68d15330c220238c54dcb0
alfgonfer <alfgonfer@alum.us.es>
2021-01-02 20:01:36 +0100

    Comentado el código del algoritmo de recuento borda para ayudar a su interpretación

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -103,6 +103,8 @@ class PostProcView(APIView):
         return Response(out)
 
     def borda(self, order_options):
+        #Creación de la salida y de una lista auxiliar para filtrar la entrada
+        #y que en la salida solo aparezca una ocurrencia por opción
         out = []
         aux = []
         for ord in order_options:
@@ -112,25 +114,33 @@ class PostProcView(APIView):
                     'postproc': 0,
                 })
             aux.append(ord['option'])
+
+        #Número de opciones distintas que hay (no de entradas)
         numOptions = max(out,key=lambda x: x['number'])['number']
 
+        #Creación de una lista que guarda de 1 a numOptions de manera inversa
+        #para después usarlo para calcular la puntuación
         puntos = [0]
         j = numOptions
         while j>=1:
             puntos.append(j)
             j-=1
 
+        #Lista que servirá para ir almacenando la suma de los votos de las distintas opciones
         votos = []
         i=0
         while i<=numOptions:
             votos.append(0)
             i+=1
 
+        #Recorrer los datos de entrada, obteniendo la opción y los votos de dicha opción
+        #en la posición seleccionada, para después multiplicarlo y obtener la puntuación real
         for ord in order_options:
             opcion = int(ord['number'])
             mult = puntos[int(ord['order_number'])]
             votos[opcion] = votos[opcion] + mult*int(ord['votes'])
 
+        #Asignar a la salida, en el parámetro postproc, la puntuación total de cada opción
         cont=0
         while cont<numOptions:
             out[cont]['postproc'] = votos[cont+1]
