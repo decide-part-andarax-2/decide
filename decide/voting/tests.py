cc5bfa5ff90df12152bdcf45247c7a8716b48eb5
javvazzam <javvazzam@alum.us.es>
2021-01-04 15:10:05 +0100

    Eliminado espacio blanco

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -146,7 +146,7 @@ class VotingTestCase(BaseTestCase):
 
         for q in v.postproc:
             self.assertEqual(tally.get(q["number"], 0), q["votes"])
- 
+
     def test_postproc_voting_compressed(self):
         v = self.create_voting()
         self.create_voters(v)
