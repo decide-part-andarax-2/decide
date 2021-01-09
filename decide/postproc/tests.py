1fd234ae70767133ef0ca579c0e9e7dfef04b99a
rafsalcas1 <rafsalcas1@alum.us.es>
2021-01-09 16:52:38 +0100

    Primer test de webster

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -721,3 +721,30 @@ class PostProcTestCase(APITestCase):
         values = response.json()
         self.assertEqual(values, expected_result)
 
+    def test_webster1(self):
+        data = {
+            'type': 'WEBSTER',
+            'seats': 10,
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 10 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 7 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 2 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 5 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 1 },
+            ]
+        }
+        expected_result = [
+            { 'option': 'Option 1', 'number': 1, 'votes': 10, 'postproc': 4 },
+            { 'option': 'Option 3', 'number': 3, 'votes': 7, 'postproc': 3 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 2 },
+            { 'option': 'Option 4', 'number': 4, 'votes': 2, 'postproc': 1 },
+            { 'option': 'Option 6', 'number': 6, 'votes': 1, 'postproc': 0 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
