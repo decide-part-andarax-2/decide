48aeca3d7aea372f1d0d781aa23ab40cd5099ba6
rafsalcas1 <rafsalcas1@alum.us.es>
2021-01-09 17:59:44 +0100

    Correción segundo test del algoritmo Webster

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -768,7 +768,7 @@ class PostProcTestCase(APITestCase):
             { 'option': 'Option 1', 'number': 1, 'votes': 10, 'postproc': 4 },
             { 'option': 'Option 6', 'number': 6, 'votes': 9, 'postproc': 4 },
             { 'option': 'Option 3', 'number': 3, 'votes': 8, 'postproc': 3 },
-            { 'option': 'Option 6', 'number': 5, 'votes': 5, 'postproc': 2 },
+            { 'option': 'Option 5', 'number': 5, 'votes': 5, 'postproc': 2 },
             { 'option': 'Option 4', 'number': 4, 'votes': 3, 'postproc': 1 },
             { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
             { 'option': 'Option 7', 'number': 7, 'votes': 0, 'postproc': 0 },
