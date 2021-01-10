fcaed12445e8ae8f836f190c4e20f03cf3673a65
jesgamlar <56022217+jesgamlar@users.noreply.github.com>
2021-01-10 11:42:26 +0100

    iniciacizacion de los tests de facebook

---
+++ decide/authentication/test_facebook.py
@@ -0,0 +1,61 @@
+
+from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from django.contrib.auth.models import User
+from base.tests import BaseTestCase
+import itertools
+import time
+from voting.models import Voting, Question, QuestionOption, QuestionOrder
+from django.conf import settings
+from mixnet.models import Auth
+from django.utils import timezone
+import logging
+
+class Facebook(StaticLiveServerTestCase):
+
+    def create_voting(self):
+        self.q = Question(desc='Prueba votación')
+        self.q.save()
+        for i in range(2):
+            opt = QuestionOption(question=self.q, option='Opción {}'.format(i+1))
+            opt.save()
+        self.v= Voting(name='Prueba votación', question=self.q, link="prueba")
+        self.v.save()
+        self.a, _ = Auth.objects.get_or_create(url=settings.BASEURL,defaults={'me': True, 'name': 'test auth'})
+        self.a.save()
+        self.v.auths.add(self.a)
+        self.v.create_pubkey()
+        self.v.start_date = timezone.now()
+        self.v.save()
+        
+
+    def setUp(self):
+
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        self.vars = {}
+        self.create_voting()
+        options = webdriver.ChromeOptions()
+        options.headless = True
+        self.driver = webdriver.Chrome(options=options)
+
+        super().setUp()
+
+    def tearDown(self):
+        super().tearDown()
+        self.driver.quit()
+        self.base.tearDown()
+        self.v.delete()
+
+
