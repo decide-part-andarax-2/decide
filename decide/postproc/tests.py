bd346836be95c7762bd89a17bb4ff5d7f958bec1
marlasesc <marlasesc@alum.us.es>
2021-01-11 11:16:40 +0100

    Cambiado la lista de resultados y añadido un par de tests de relativa

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -142,6 +142,7 @@ class PostProcTestCase(APITestCase):
         expected_result = [
             { 'option': 'Option 1', 'number': 1, 'votes': 7, 'postproc': 1 },
             { 'option': 'Option 3', 'number': 3, 'votes': 6, 'postproc': 0 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 4, 'postproc': 0 },
         ]
 
         response = self.client.post('/postproc/', data, format='json')
@@ -183,6 +184,8 @@ class PostProcTestCase(APITestCase):
         expected_result = [
             { 'option': 'Option 4', 'number': 4, 'votes': 4, 'postproc': 1 },
             { 'option': 'Option 3', 'number': 3, 'votes': 3, 'postproc': 0 },
+            { 'option': 'Option 2', 'number': 2, 'votes': 2, 'postproc': 0 },
+            { 'option': 'Option 1', 'number': 1, 'votes': 1, 'postproc': 0 },
         ]
 
         response = self.client.post('/postproc/', data, format='json')
@@ -215,6 +218,79 @@ class PostProcTestCase(APITestCase):
         self.assertEqual(response.status_code, 200)
         values = response.json()
         self.assertEqual(values, expected_result)
+
+    def test_relativa5(self):
+        data = {
+            'type': 'RELATIVA',
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 0 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 0 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 0 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 0 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 0 },
+                { 'option': 'Option 7', 'number': 7, 'votes': 0 },
+                { 'option': 'Option 8', 'number': 8, 'votes': 0 },
+                { 'option': 'Option 9', 'number': 9, 'votes': 1 },
+                { 'option': 'Option 10', 'number': 10, 'votes': 0 },
+                { 'option': 'Option 11', 'number': 11, 'votes': 0 },
+                { 'option': 'Option 12', 'number': 12, 'votes': 0 },
+                { 'option': 'Option 13', 'number': 13, 'votes': 0 },
+                { 'option': 'Option 14', 'number': 14, 'votes': 0 },
+                { 'option': 'Option 15', 'number': 15, 'votes': 0 },
+            ]
+        }
+
+        expected_result = [
+                { 'option': 'Option 9', 'number':9, 'votes': 1, 'postproc': 1 },
+                { 'option': 'Option 1', 'number': 1, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 7', 'number': 7, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 8', 'number': 8, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 10', 'number': 10, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 11', 'number': 11, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 12', 'number': 12, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 13', 'number': 13, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 14', 'number': 14, 'votes': 0, 'postproc': 0 },
+                { 'option': 'Option 15', 'number': 15, 'votes': 0, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        values = response.json()
+        self.assertEqual(values, expected_result)
+
+    def test_relativa6(self):
+        data = {
+            'type': 'RELATIVA',
+            'options': [
+                { 'option': 'Option 1', 'number': 1, 'votes': 70 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 800 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 345 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 200 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 729 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 273},
+            ]
+        }
+
+        expected_result = [
+                { 'option': 'Option 2', 'number': 2, 'votes': 800, 'postproc': 1 },
+                { 'option': 'Option 5', 'number': 5, 'votes': 729, 'postproc': 0 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 345, 'postproc': 0 },
+                { 'option': 'Option 6', 'number': 6, 'votes': 273, 'postproc': 0 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 200, 'postproc': 0 },
+                { 'option': 'Option 1', 'number': 1, 'votes': 70, 'postproc': 0 },
+        ]
+
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+        values = response.json()
+        self.assertEqual(values, expected_result)
+        
 
     def test_absoluta1(self):
         data = {
