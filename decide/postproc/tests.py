2e69d6e7b47dd48ef4b72926250e6ada4dea287e
rafsalcas1 <rafsalcas1@alum.us.es>
2021-01-26 17:54:16 +0100

    Añadido tests de algoritmo webster modificado

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -1343,3 +1343,41 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+    def test_webster_modificado_1(self):
+            data = {
+                'type': 'WEBSTERMOD',
+                'seats': 8,
+                'options': [
+                    { 'option': 'Option 1', 'number': 1, 'votes': 10 },
+                    { 'option': 'Option 2', 'number': 2, 'votes': 0 },
+                    { 'option': 'Option 3', 'number': 3, 'votes': 7 },
+                    { 'option': 'Option 4', 'number': 4, 'votes': 5 },
+                ]
+            }
+            expected_result = [
+                { 'option': 'Option 1', 'number': 1, 'votes': 10, 'postproc': 4 },
+                { 'option': 'Option 3', 'number': 3, 'votes': 7, 'postproc': 2 },
+                { 'option': 'Option 4', 'number': 4, 'votes': 5, 'postproc': 2 },
+                { 'option': 'Option 2', 'number': 2, 'votes': 0, 'postproc': 0 },
+            ]
+
+            response = self.client.post('/postproc/', data, format='json')
+            self.assertEqual(response.status_code, 200)
+
+            values = response.json()
+            self.assertEqual(values, expected_result)
+
+    def test_no_options_webter_modificado(self):
+            seats = 5
+            data = {
+                'type': 'WEBSTERMOD',
+                'seats': seats
+            }
+            expected_result = {}
+
+            response = self.client.post('/postproc/', data, format='json')
+            self.assertEqual(response.status_code, 400)
+
+            values = response.json()
+            self.assertEqual(values, expected_result)
